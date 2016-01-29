fb7da31de042656c059e8fcf849c625ddaac1e29
Rasmus Pettersson Vik <rasmus.pettersson.vik@gmail.com>
2016-01-29 13:41:40 +0100

    Added handling for getting kicked and getting invited to channel

--- yxbot.py
+++ yxbot.py
@@ -42,6 +42,7 @@ class YxBot:
 		self.running = True
 		self.flags.setFlag("CAN_QUIT", False)
 		self.flags.setFlag("RAW_ENABLED", False)
+		self.flags.setFlag("IN_CHANNEL", False)
 		self._load()
 
 	def _load(self):
@@ -80,8 +81,23 @@ class YxBot:
 					  +"NICK " + self._nick() + "\n")
 
 	def _joinChannel(self):
-		print "-- Joining channel " + self._channel() + " --\n"
-		self.sock.send("JOIN " + self._channel() + "\n")
+		if not self.flags.getFlag("IN_CHANNEL"):
+			print "-- Joining channel " + self._channel() + " --\n"
+			self.sock.send("JOIN " + self._channel() + "\n")
+			self.flags.setFlag("IN_CHANNEL", True)
+		else:
+			print "-- Already in Channel --\n"
+
+	def _leaveChannel(self):
+		if self.flags.getFlag("IN_CHANNEL"):
+			print "-- Leaving channel --\n"
+			self.sock.send("PART\n")
+		else:
+			print "-- Not in channel --\n"
+
+	def _changeChannel(self, chan):
+		print "-- Changing channel flag to " + chan + " --\n"
+		self.flags.setFlag("CHANNEL", chan)
 
 	def _sendMessage(self, message, action=False):
 		if self.flags.getFlag("SILENT"):
@@ -152,10 +168,13 @@ class YxBot:
 		if message.find(":" + self.flags.getFlag("ADMIN") + "!") != -1:
 			if message.find(self._nick() + ": quit") != -1 and self.flags.getFlag("CAN_QUIT"):
 				self.disconnect()
+
 			if message.find(self._nick() + ": reload") != -1:
 				self._load()
+
 			if message.find(self._nick() + ": count") != -1:
 				self._sendMessage("Fabrikat: " + str(len(self.yxfabrikat)) + ", Typer: " + str(len(self.yxtyp)) + ", Kroppsdelar: " + str(len(self.kroppsdel)))
+
 			if message.find(self._nick() + ": raw") != -1 and self.flags.getFlag("RAW_ENABLED"):
 				text = message[message.find(": raw"):]
 				text = text.split(": raw")
@@ -166,9 +185,11 @@ class YxBot:
 			#EDITING FLAGS FROM CHAT
 			if message.find(self._nick() + ": setFlag") != -1 or message.find(self._nick() + ": getFlag") != -1 or message.find(self._nick() + ": delFlag") != -1:
 				print "-- EDITING FLAGS FROM CHAT --\n"
+
 				if message.find(self._nick() + ": setFlag") != -1:
 					print "-- SET FLAG --\n"
 					splitted = message[message.find("setFlag"):].split()
+
 					if len(splitted) == 4: #1 setFlag, 2 flag, 3 type, 4 value ex, setFlag SILENT BOOL True => ['setFlag', 'SILENT', 'BOOL', 'True']
 						if splitted[2] == "STR":
 							self.flags.setFlag(splitted[1], splitted[3])
@@ -189,17 +210,34 @@ class YxBot:
 				if message.find(self._nick() + ": getFlag") != -1:
 					print "-- GET FLAG --\n"
 					splitted = message[message.find("getFlag"):].split()
+
 					if len(splitted) == 2: #1 getFlag, 2 flag
 						self._sendMessage(splitted[1] + " : " + str(self.flags.getFlag(splitted[1])))
+
 					print self.flags._flags
 
 				if message.find(self._nick() + ": delFlag") != -1:
 					print "-- DEL FLAG --\n"
 					splitted = message[message.find("delFlag"):].split()
+
 					if len(splitted) == 2: #1 delFlag, 2 flag
 						self.flags.removeFlag(splitted[1])
 			#END EDIT FLAGS
 		#END ADMIN COMMANDS
+
+		if message.lower().find(str.lower("KICK " + self.flags.getFlag("CHANNEL") + " " + self._nick())) != -1:
+			print "-- Kicked from channel --\n"
+			self.flags.setFlag("IN_CHANNEL", False)
+
+		#:Armandur!~Rasmus@c-c6c1e055.03-48-68736410.cust.bredbandsbolaget.se INVITE YxBot :#armandur_test
+		if message.find("INVITE " + self._nick()) != -1 and not self.flags.getFlag("IN_CHANNEL"):
+			splitted = message[message.find("INVITE " + self._nick() + " :"):].split()
+
+			if len(splitted) == 3:
+				chan = splitted[2][1:]
+				print "-- Received INVITE to " + chan + " --\n"
+				self._changeChannel(chan)
+				self._joinChannel()
 
 		if message.find("Closing link") != -1 and message.find(self._nick()) != -1:
 			self.disconnect();
@@ -258,8 +296,6 @@ class YxBot:
 		print "-- Loop stopped -- \n"
 
 random.seed()
-#bot = YxBot(("irc.snoonet.org", 6667), "#sweden", "YxBot", "Armandur", ["yxfabrikat.txt", "yxtyp.txt", "kroppsdel.txt"])
-
 flags = Flags()
 # flags.setFlag("CONNECTION", ("portlane.se.quakenet.org", 6667))
 # flags.setFlag("CHANNEL", "#anrop.net")
@@ -267,7 +303,7 @@ flags = Flags()
 # flags.setFlag("PATHS", ["yxfabrikat.txt", "yxtyp.txt", "kroppsdel.txt"])
 # flags.setFlag("ADMIN", "Armandur")
 # flags.setFlag("SILENT", False)
-# flags.setFlag("ONLYINCHANNEL", False)
+# flags.setFlag("USERS_ONLY", False)
 
 flags.setFlag("CONNECTION", ("irc.snoonet.org", 6667))
 flags.setFlag("CHANNEL", "#sweden")
@@ -278,12 +314,12 @@ flags.setFlag("SILENT", False)
 flags.setFlag("USERS_ONLY", False)
 
 # flags.setFlag("CONNECTION", ("irc.oftc.net", 6667))
-# flags.setFlag("CHANNEL", "#armandur")
+# flags.setFlag("CHANNEL", "#armandur_test")
 # flags.setFlag("NICK", "YxBot")
 # flags.setFlag("PATHS", ["yxfabrikat.txt", "yxtyp.txt", "kroppsdel.txt"])
 # flags.setFlag("ADMIN", "Armandur")
 # flags.setFlag("SILENT", False)
-# flags.setFlag("ONLYINCHANNEL", False)
+# flags.setFlag("USERS_ONLY", False)
 
 bot = YxBot(flags)
 bot.connect()
