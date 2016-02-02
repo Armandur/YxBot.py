146c5ae5779d13651db1d746c4e382d3cb5855a6
Rasmus Pettersson Vik <rasmus.pettersson.vik@gmail.com>
2016-02-02 13:51:31 +0100

    fix

--- handlers/InviteHandler.py
+++ handlers/InviteHandler.py
@@ -5,7 +5,7 @@ class InviteHandler(MessageHandler):
         def handle_message(self, message):
             #:Armandur!~Rasmus@c-c6c1e055.03-48-68736410.cust.bredbandsbolaget.se INVITE YxBot :#armandur_test
             if message.find("INVITE " + self.bot._nick()) != -1 and not self.flags.getFlag("IN_CHANNEL"):
-                splitted = message[message.find("INVITE " + self._nick() + " :"):].split()
+                splitted = message[message.find("INVITE " + self.bot._nick() + " :"):].split()
 
                 if len(splitted) == 3:
                     chan = splitted[2][1:]
