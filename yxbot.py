cc4418d416a62101cebb73146d66d1bc1eee344f
Rasmus Pettersson Vik <rasmus.pettersson.vik@gmail.com>
2016-02-02 02:57:44 +0100

    splitting

--- yxbot.py
+++ yxbot.py
@@ -270,33 +270,3 @@ class YxBot:
 					buff = ""
 
 		print "-- Loop stopped -- \n"
-
-random.seed()
-flags = Flags.Flags()
-
-# flags.setFlag("CONNECTION", ("portlane.se.quakenet.org", 6667))
-# flags.setFlag("CHANNEL", "#anrop.net")
-# flags.setFlag("NICK", "Yxbotten")
-# flags.setFlag("PATHS", ["yxfabrikat.txt", "yxtyp.txt", "kroppsdel.txt"])
-# flags.setFlag("ADMIN", "Armandur")
-# flags.setFlag("SILENT", False)
-# flags.setFlag("USERS_ONLY", False)
-
-# flags.setFlag("CONNECTION", ("irc.snoonet.org", 6667))
-# flags.setFlag("CHANNEL", "#sweden")
-# flags.setFlag("NICK", "YxBot")
-# flags.setFlag("PATHS", ["yxfabrikat.txt", "yxtyp.txt", "kroppsdel.txt"])
-# flags.setFlag("ADMIN", "Armandur")
-# flags.setFlag("SILENT", False)
-# flags.setFlag("USERS_ONLY", False)
-
-flags.setFlag("CONNECTION", ("irc.oftc.net", 6667))
-flags.setFlag("CHANNEL", "#armandur_test")
-flags.setFlag("NICK", "YxBot")
-flags.setFlag("PATHS", ["yxfabrikat.txt", "yxtyp.txt", "kroppsdel.txt"])
-flags.setFlag("ADMIN", "Armandur")
-flags.setFlag("SILENT", False)
-flags.setFlag("USERS_ONLY", False)
-
-bot = YxBot(flags)
-bot.connect()
