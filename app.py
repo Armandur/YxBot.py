d0caced173ac951f5373e85d9f4e15b55edd037c
Rasmus Pettersson Vik <rasmus.pettersson.vik@gmail.com>
2016-12-27 20:42:40 +0100

    removed unneccesary configuration flags

--- app.py
+++ app.py
@@ -3,31 +3,5 @@ from yxbot import YxBot
 from Flags import Flags
 
 random.seed()
-# flags = Flags()
-
-# flags.setFlag("CONNECTION", ("portlane.se.quakenet.org", 6667))
-# flags.setFlag("CHANNEL", "#anrop.net")
-# flags.setFlag("NICK", "Yxbotten")
-# flags.setFlag("PATHS", ["yxfabrikat.txt", "yxtyp.txt", "kroppsdel.txt"])
-# flags.setFlag("ADMIN", "Armandur")
-# flags.setFlag("SILENT", False)
-# flags.setFlag("USERS_ONLY", False)
-
-# flags.setFlag("CONNECTION", ("irc.snoonet.org", 6667))
-# flags.setFlag("CHANNEL", "#sweden")
-# flags.setFlag("NICK", "YxBot")
-# flags.setFlag("PATHS", ["yxfabrikat.txt", "yxtyp.txt", "kroppsdel.txt"])
-# flags.setFlag("ADMIN", "Armandur")
-# flags.setFlag("SILENT", False)
-# flags.setFlag("USERS_ONLY", False)
-
-# flags.setFlag("CONNECTION", ("irc.oftc.net", 6667))
-# flags.setFlag("CHANNEL", "#armandur_test")
-# flags.setFlag("NICK", "YxBot")
-# flags.setFlag("PATHS", ["yxfabrikat.txt", "yxtyp.txt", "kroppsdel.txt"])
-# flags.setFlag("ADMIN", "Armandur")
-# flags.setFlag("SILENT", False)
-# flags.setFlag("USERS_ONLY", False)
-
 bot = YxBot()
 bot.connect()
